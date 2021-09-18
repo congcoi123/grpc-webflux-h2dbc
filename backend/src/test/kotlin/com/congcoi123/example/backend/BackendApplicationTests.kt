136d57489717c70a357b5f8a7a4cbcf3843662ff
Kong <congcoi123@gmail.com>
2021-09-18 09:47:41 +0700

    First initialization

---
+++ backend/src/test/kotlin/com/congcoi123/example/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.congcoi123.example.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
